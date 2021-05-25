e2c776d34d838a7f1171f8ba99ab36412d6a6ddd
Malik <malik.lukmanov@gmail.com>
2021-05-26 02:32:55 +0500

    smartfox

---
+++ src/main/java/smartfox/Overview.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * BlueBox - технология http-туннелирования, которая позволяет клиентам подключаться из-за брэндмауэров, проксей и т д, когда
+ * соект-соединение недоступно. Типично для офисов, школ и других учреждений, где сетевой трафик ограничен несколькими выбранными
+ * сервисами. Позволяет игрокам из закрытых сетей играть в мультиплеер.
+ *      http://docs2x.smartfoxserver.com/GettingStarted/bluebox
+ * HRC+ - High Resilient Connection (высоконадежное соединение). Позволяет игрокам возвращаться в игру без потери их актуального
+ * состояния после внезапного дисконнекта. Недоступно для HTML5 клиентов.
+ *      http://docs2x.smartfoxserver.com/GettingStarted/reconnection-hrc-plus
+ * Шифрование (криптография) - есть возможно включить TLS-шифрование для всего трафика, проходящего через SmartFoxServer.
+ *      http://docs2x.smartfoxserver.com/GettingStarted/cryptography
+ * Логи - просто логи. http://docs2x.smartfoxserver.com/GettingStarted/log-configuration
+ * RedBox - бесплатный аддон для SmartFoxServer, который добавляет возможность аудио- и видеостриминга серверу.
+ *      http://docs2x.smartfoxserver.com/GettingStarted/redbox
+ *
+ * */
+
+package smartfox;
+
+public class Overview {
+}
