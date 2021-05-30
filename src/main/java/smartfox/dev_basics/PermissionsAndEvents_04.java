9bf898fb18bee7f6752fc4ce89a9b61ae06b0c85
Malik <malik.lukmanov@gmail.com>
2021-05-31 00:12:12 +0500

    smartfox

---
+++ src/main/java/smartfox/dev_basics/PermissionsAndEvents_04.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * Доп. настройки. Доступы и события. Пример конфигурации комнаты в админке (sm_room_cfg.png).
+ *
+ * */
+
+package smartfox.dev_basics;
+
+public class PermissionsAndEvents_04 {
+}
---
+++ src/main/java/smartfox/overview/OverviewRest_02.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * Для аналитики смартфокс собирает много разной информации. Подробнее здесь http://docs2x.smartfoxserver.com/Overview/privacy.
+ * Статьи о:
+ *  - производительности и масштабируемости
+ *  - архитектуре сервера
+ *  - безопасности
+ *  - старте 100к параллельных пользователей
+ *  - интеграции с Hazelcast
+ *  - не пойми о чем An App, a Marathon and an Astronaut
+ * находятся здесь http://docs2x.smartfoxserver.com/Overview/white-papers.
+ *
+ * */
+
+package smartfox.overview;
+
+public class OverviewRest_02 {
+}
