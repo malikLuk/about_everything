6a9652e629b4a16222cabefff55a7f0350227211
Malik <malik.lukmanov@gmail.com>
2021-06-07 18:20:38 +0500

    smartfox

---
+++ src/main/java/smartfox/java_extensions/FirstExtension_02.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * Пример экстеншена. Проект D:/IdeaProjects/first_extension и D:/IdeaProjects/sm_first_ext
+ * Важно. Имя джарника Экстеншена должно заканчиваться на Extension, это нужно, чтобы админка его распознала как Экстеншен. Конфигуратор Зон и Комнат
+ * http://docs2x.smartfoxserver.com/GettingStarted/admintool-ZoneConfigurator-room.
+ * Также важно, чтобы в джарнике не лежало никаких доп. либ - они должны быть либо в папке Экстеншена либо в папке __libs__ для всех Экстеншенов
+ *
+ * */
+
+package smartfox.java_extensions;
+
+public class FirstExtension_02 {
+}
